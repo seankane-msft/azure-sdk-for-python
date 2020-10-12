eaf182aef95d0e70f3473d9d7aa0916532faaf89
seankane-msft <seankane@microsoft.com>
2020-10-12 16:03:08 -0700

    started tinkering with a .env file in the root and using that for mgmt_settings_real

--- tools/azure-sdk-tools/devtools_testutils/mgmt_settings_fake.py
+++ tools/azure-sdk-tools/devtools_testutils/mgmt_settings_fake.py
@@ -10,7 +10,7 @@ SUBSCRIPTION_ID = "00000000-0000-0000-0000-000000000000"
 # Note: Keyvault tests require EnvironmentCredential variables to be populated as well. (AZURE_CLIENT_ID etc)
 TENANT_ID = '00000000-0000-0000-0000-000000000000'
 # Note: Not the OID from the azure portal.  Use 'az ad sp show --id <id> | grep objectId'
-CLIENT_OID = '00000000-0000-0000-0000-000000000000' 
+CLIENT_OID = '00000000-0000-0000-0000-000000000000'
 # Cognitive Services tests
 CS_SUBSCRIPTION_KEY = '0000000000000000000000000000'
 # Event Grid key
@@ -30,6 +30,14 @@ IS_SERVER_SIDE_FILE_ENCRYPTION_ENABLED = True
 ENABLE_LOGGING = True
 AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=storagename;AccountKey=NzhL3hKZbJBuJ2484dPTR+xF30kYaWSSCbs2BzLgVVI1woqeST/1IgqaLm6QAOTxtGvxctSNbIR/1hW8yH+bJg==;EndpointSuffix=core.windows.net'
 
+def load_dotenv_file():
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    abspath = os.path.abspath(__file__)
+    dotenv_path = os.path.realpath(os.path.join(abspath, "../../../../.env"))
+    load_dotenv(dotenv_path=dotenv_path)
+    print(os.environ['AZ_SUBSCRIPTION_ID'], dotenv_path)
+
 # Read for details of this file:
 # https://github.com/Azure/azure-sdk-for-python/wiki/Contributing-to-the-tests
 
@@ -37,10 +45,11 @@ def get_azure_core_credentials(**kwargs):
     # from azure.identity import (
     #     ClientSecretCredential
     # )
+    # load_dotenv_file()
     # return ClientSecretCredential(
-    #     client_id = '<AAD App client id>',
-    #     client_secret = '<secret for the aad app>',
-    #     tenant_id = '<microsoft aad tenant id>'
+    #     client_id = os.environ['AZURE_CLIENT_ID']
+    #     client_secret = os.environ['AZURE_CLIENT_SECRET']
+    #     tenant_id = os.environ['AZURE_TENANT_ID']
     # )
     # Needed to play recorded tests
     from azure.core.credentials import AccessToken
