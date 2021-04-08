48c48dfac9b5d8f5a4c7abf78320d1d10dd057fa
seankane-msft <seankane@microsoft.com>
2021-04-08 14:57:49 -0400

    updating generated and swagger

--- sdk/containerregistry/azure-containerregistry/azure/containerregistry/_generated/aio/_container_registry.py
+++ sdk/containerregistry/azure-containerregistry/azure/containerregistry/_generated/aio/_container_registry.py
@@ -4,31 +4,23 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from typing import Any
+from copy import deepcopy
+from typing import Any, TYPE_CHECKING
 
+from azure.containerregistry.core.rest import AsyncHttpResponse, HttpRequest
 from azure.core import AsyncPipelineClient
-from azure.core.pipeline.transport import AsyncHttpResponse, HttpRequest
 from msrest import Deserializer, Serializer
 
+if TYPE_CHECKING:
+    # pylint: disable=unused-import,ungrouped-imports
+    from typing import Dict
+
 from ._configuration import ContainerRegistryConfiguration
-from .operations import ContainerRegistryOperations
-from .operations import ContainerRegistryRepositoryOperations
-from .operations import ContainerRegistryBlobOperations
-from .operations import AuthenticationOperations
-from .. import models
 
 
 class ContainerRegistry(object):
     """Metadata API definition for the Azure Container Registry runtime.
 
-    :ivar container_registry: ContainerRegistryOperations operations
-    :vartype container_registry: container_registry.aio.operations.ContainerRegistryOperations
-    :ivar container_registry_repository: ContainerRegistryRepositoryOperations operations
-    :vartype container_registry_repository: container_registry.aio.operations.ContainerRegistryRepositoryOperations
-    :ivar container_registry_blob: ContainerRegistryBlobOperations operations
-    :vartype container_registry_blob: container_registry.aio.operations.ContainerRegistryBlobOperations
-    :ivar authentication: AuthenticationOperations operations
-    :vartype authentication: container_registry.aio.operations.AuthenticationOperations
     :param url: Registry login URL.
     :type url: str
     """
@@ -42,36 +34,44 @@ class ContainerRegistry(object):
         self._config = ContainerRegistryConfiguration(url, **kwargs)
         self._client = AsyncPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
-        client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
-        self._serialize = Serializer(client_models)
+        self._serialize = Serializer()
         self._serialize.client_side_validation = False
-        self._deserialize = Deserializer(client_models)
 
-        self.container_registry = ContainerRegistryOperations(
-            self._client, self._config, self._serialize, self._deserialize)
-        self.container_registry_repository = ContainerRegistryRepositoryOperations(
-            self._client, self._config, self._serialize, self._deserialize)
-        self.container_registry_blob = ContainerRegistryBlobOperations(
-            self._client, self._config, self._serialize, self._deserialize)
-        self.authentication = AuthenticationOperations(
-            self._client, self._config, self._serialize, self._deserialize)
-
-    async def _send_request(self, http_request: HttpRequest, **kwargs: Any) -> AsyncHttpResponse:
+    async def send_request(self, http_request: HttpRequest, **kwargs: Any) -> AsyncHttpResponse:
         """Runs the network request through the client's chained policies.
 
+        We have helper methods to create requests specific to this service in `azure.containerregistry.rest`.
+        Use these helper methods to create the request you pass to this method. See our example below:
+
+        >>> from azure.containerregistry.rest import build_containerregistry_check_docker_v2_support_request
+        >>> request = build_containerregistry_check_docker_v2_support_request()
+        <HttpRequest [GET], url: '/v2/'>
+        >>> response = await client.send_request(request)
+        <AsyncHttpResponse: 200 OK>
+
+        For more information on this code flow, see https://aka.ms/azsdk/python/llcwiki
+
+        For advanced cases, you can also create your own :class:`~azure.containerregistry.core.rest.HttpRequest`
+        and pass it in.
+
         :param http_request: The network request you want to make. Required.
-        :type http_request: ~azure.core.pipeline.transport.HttpRequest
-        :keyword bool stream: Whether the response payload will be streamed. Defaults to True.
+        :type http_request: ~azure.containerregistry.core.rest.HttpRequest
+        :keyword bool stream_response: Whether the response payload will be streamed. Defaults to False.
         :return: The response of your network call. Does not do error handling on your response.
-        :rtype: ~azure.core.pipeline.transport.AsyncHttpResponse
+        :rtype: ~azure.containerregistry.core.rest.AsyncHttpResponse
         """
+        request_copy = deepcopy(http_request)
         path_format_arguments = {
             'url': self._serialize.url("self._config.url", self._config.url, 'str', skip_quote=True),
         }
-        http_request.url = self._client.format_url(http_request.url, **path_format_arguments)
-        stream = kwargs.pop("stream", True)
-        pipeline_response = await self._client._pipeline.run(http_request, stream=stream, **kwargs)
-        return pipeline_response.http_response
+        request_copy.url = self._client.format_url(request_copy.url, **path_format_arguments)
+        stream_response = kwargs.pop("stream_response", False)
+        pipeline_response = await self._client._pipeline.run(request_copy._internal_request, stream=stream_response, **kwargs)
+        return AsyncHttpResponse(
+            status_code=pipeline_response.http_response.status_code,
+            request=request_copy,
+            _internal_response=pipeline_response.http_response
+        )
 
     async def close(self) -> None:
         await self._client.close()
--- sdk/containerregistry/azure-containerregistry/swagger/README.md
+++ sdk/containerregistry/azure-containerregistry/swagger/README.md
@@ -5,12 +5,12 @@
 input-file: https://raw.githubusercontent.com/Azure/azure-sdk-for-js/e40f55d5fa64d177c0267b52129c77aa353f95d8/sdk/containerregistry/container-registry/swagger/containerregistry.json
 output-folder: "../azure/containerregistry/_generated"
 namespace: azure.containerregistry
+no-namespace-folders: true
 ```
 
 <!-- ```
 input-file: https://raw.githubusercontent.com/Azure/azure-sdk-for-js/1138db4e60f389332df553686145356cce0b79ed/sdk/containerregistry/container-registry/swagger/containerregistry.json
 output-folder: "../azure/containerregistry/_generated"
-no-namespace-folders: true
 python: true
 python-mode: create
 license-header: MICROSOFT_MIT_NO_VERSION
